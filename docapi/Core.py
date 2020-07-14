092fcca5e6b62cc7df198db88778d3063c9150eb
Sabbir-Amin <sabbir@rokomari.com>
2020-07-15 02:25:38 +0600

    Adding Files

--- docapi/Core.py
+++ docapi/Core.py
@@ -10,9 +10,11 @@ email: sabbir@rokomari.com, sabbiramin.cse11ruet@gmail.com
 import datetime
 
 
+
 class Doc:
     def __init__(self, name):
         self.name = name
+        self.base_url = 'someid:'
         self.today = datetime.datetime.now().strftime('%Y_%d%b_%I_%M_%p')
         print(self.today)
         self.apis = list()
@@ -21,7 +23,27 @@ class Doc:
         return self.apis.append(api)
 
     def build(self):
-        pass
+        api_list = list()
+        for api in self.apis:
+            api_name = '## {}'.format(api.name)
+            api_details = '{}'.format(api.details)
+            div1  = '---------------'
+            url = '###\'URL\'\n{}{}'.format(self.base_url, api.url)
+
+            Req = '## Request'
+            div2 = '---------------'
+
+            Header_key= '### Header'
+            Header_val = '```json' \
+                         '{}' \
+                         '```'.format(api.request.header)
+            Param_key = '### Parameters'
+            Param_vals = '```json' \
+                         '{}' \
+                         '```'.format(api.request.params)
+            complete = '\n'.join([api_name,api_details,div1,url,Req,div2,Header_key,
+                                  Header_val,Param_key,Param_vals])
+            print (complete)
 
 
 class Request:
@@ -40,18 +62,20 @@ class Response:
 
 
 class API:
-    def __init__(self):
+    def __init__(self, req, res):
         self.url = ''
         self.name = ''
         self.details = ''
-        self.request = None
-        self.response = None
+        self.request = req
+        self.response = res
 
 
 if __name__ == '__main__':
     doc = Doc(name='My API List')
+    req, res = Request(),Response()
 
-    get_book_by_id = API()
+    get_book_by_id = API(req, res)
+    get_book_by_id.url = '/api/books/id'
     get_book_by_id.details = 'This is the API is about getting book details by ID'
     get_book_by_id.request.header ={'http-api-key':'nksks.wlekjlk43k3isdslk3'}
     get_book_by_id.request.params = {'id':1,'page':1,'size':200}
@@ -60,15 +84,4 @@ if __name__ == '__main__':
 
     doc.add_api(get_book_by_id)
 
-
-
-
-
-
-
-
-    req = Request()
-    req.header = {
-        'http-api-key': 'bhslks.skjsjsliepoqwjij3oi3jsjd'
-    }
-    print(req.header)
+    doc.build()
