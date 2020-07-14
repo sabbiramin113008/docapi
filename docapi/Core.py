23dff1a13ab1709b96d395eeeb631abc0ea5a17c
Sabbir-Amin <sabbir@rokomari.com>
2020-07-14 22:40:14 +0600

    Adding Files

--- docapi/Core.py
+++ docapi/Core.py
@@ -9,28 +9,37 @@ email: sabbir@rokomari.com, sabbiramin.cse11ruet@gmail.com
 
 import datetime
 
-class Doc():
-    def __init__(self,name):
+
+class Doc:
+    def __init__(self, name):
         self.name = name
         self.today = datetime.datetime.now().strftime('%Y_%d%b_%I_%M_%p')
-        print (self.today)
+        print(self.today)
         self.apis = list()
 
+    def add_api(self, api):
+        return self.apis.append(api)
+
+    def build(self):
+        pass
 
 
-class Request():
-    def __init__(self):
-        self.header = dict()
+class Request:
+    def __init__(self, ):
+        self.header = {}
         self.body = None
 
-class Response():
+
+class Response:
     def __init__(self):
         self.header = dict()
         self.body = None
         self.status_code = 200
 
-class API():
-    def __init__(self, url,name, details, request, response):
+
+
+class API:
+    def __init__(self, url, name, details, request, response):
         self.url = url
         self.name = name
         self.details = ''
@@ -38,10 +47,11 @@ class API():
         self.response = response
 
 
-
-
-
-
-if __name__=='__main__':
-
+if __name__ == '__main__':
     doc = Doc(name='My API List')
+
+    req = Request()
+    req.header = {
+        'http-api-key': 'bhslks.skjsjsliepoqwjij3oi3jsjd'
+    }
+    print(req.header)
