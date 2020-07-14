55afbf0bcb0c387c69531e003b431edd49a43d4f
Sabbir-Amin <sabbir@rokomari.com>
2020-07-14 23:23:34 +0600

    Adding Files

--- docapi/Core.py
+++ docapi/Core.py
@@ -27,6 +27,7 @@ class Doc:
 class Request:
     def __init__(self, ):
         self.header = {}
+        self.params = None
         self.body = None
 
 
@@ -39,16 +40,32 @@ class Response:
 
 
 class API:
-    def __init__(self, url, name, details, request, response):
-        self.url = url
-        self.name = name
+    def __init__(self):
+        self.url = ''
+        self.name = ''
         self.details = ''
-        self.request = request
-        self.response = response
+        self.request = None
+        self.response = None
 
 
 if __name__ == '__main__':
     doc = Doc(name='My API List')
+
+    get_book_by_id = API()
+    get_book_by_id.details = 'This is the API is about getting book details by ID'
+    get_book_by_id.request.header ={'http-api-key':'nksks.wlekjlk43k3isdslk3'}
+    get_book_by_id.request.params = {'id':1,'page':1,'size':200}
+    get_book_by_id.response.status_code = 200
+    get_book_by_id.response.body = {'flag':1,'response':'kl'}
+
+    doc.add_api(get_book_by_id)
+
+
+
+
+
+
+
 
     req = Request()
     req.header = {
